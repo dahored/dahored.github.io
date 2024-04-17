00c5f25e3e5b102203e73b190bffac31933ef5f9
Diego Hernandez <dhernandez@playvox.com>
2024-04-16 23:27:02 -0500

    feat: Add support for images #7

--- src/app/modules/main-banner/index.tsx
+++ src/app/modules/main-banner/index.tsx
@@ -1,16 +1,48 @@
-import GlobalConfig from "@/app/app.config";
 import styles from "./styles.module.scss";
 import Title from "@/app/components/title";
+import Image from "next/image";
+import CSS from 'csstype';
 
 type Props = {
-  // children: any
-  name: string
+  title: string,
+  subtitle?: string,
+  image?: string
+  imageType?: 'circle' | 'square' | 'rounded',
+  background?: string,
+  wave?: boolean,
+  styles?: object
+}
+
+function getMainBannerStyles(props: Props): CSS.Properties {
+  return {
+    ...props.styles,
+  }
 }
 
 export default function MainBanner(props: Props) {
-  return <div className={`${styles.mainBanner}`}>
+  return <div className={`${styles.mainBanner}`} style={getMainBannerStyles(props)}>
     <div className={`${styles.mainBannerWrapper}`}>
-      <Title title={props.name} variant="h2" light></Title>
+      <div className="responsive-content">
+        {props.image && 
+        <Image
+            src={props.image}
+            alt="Main photo"
+            className={`${styles.image} ${styles[`image--${props.imageType}`] || styles[`image--square`]}`}
+            width={200}
+            height={200}
+            priority
+            objectFit="cover"
+        />
+        }
+        <Title title={props.title} variant="h1"></Title>
+        {props.subtitle && <Title title={props.subtitle} variant="h3" light></Title>}
+      </div>
+    </div>
+
+    <div className="waves">
+      {props.wave && <div className={`${styles.wave} ${styles.wave1}`}></div>}
+      {props.wave && <div className={`${styles.wave} ${styles.wave2}`}></div>}
+      {props.wave && <div className={`${styles.wave} ${styles.wave3}`}></div>}
     </div>
   </div>;
 }
